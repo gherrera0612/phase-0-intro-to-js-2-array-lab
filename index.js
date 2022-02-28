4f11d40e80ad2741e97ef075fe0c2c8f70ab2798
Trey Herrera <therrera152@gmail.com>
2022-02-27 18:53:03 -0600

    commit tree 4cdb3a65770ffa3888d76203ca62be70bef80357

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+let cats = ["Milo" , "Otis" , "Garfield"];
+
+function destructivelyAppendCat (cat) {
+    cats.push(cat);
+}
+// adds cat name to the end of the array
+function destructivelyPrependCat (cat) {
+    cats.unshift(cat);
+}
+//adds cat name to the beginning of the array
+
+function destructivelyRemoveLastCat (cat) {
+    cats.pop(cat);
+}
+//removes the last cat
+
+function destructivelyRemoveFirstCat(cat){
+    cats.shift(cat);
+}
+//removes the first cat
+
+function appendCat(cat){
+    return [...cats, cat]
+}
+// adds a cat to the endand keeps the array the same 
+//... is spread operator
+
+function prependCat(cat){
+    return [cat, ... cats]
+}
+//adds a cat to the beginning and keeps the array the same
+
+function removeLastCat (cat) {
+    return cats.slice(0, cats.length -1);
+}
+//removes the last cat in the array
+
+function removeFirstCat (cat){
+    return cats.slice(1);
+}
